1cb1f79c134fe337f73bb783233b589d8c157aa1
Kororu <68053894+Kororu-lab@users.noreply.github.com>
2023-09-13 21:44:33 +0900

    Update functions.py
    answer

--- customfunction/functions.py
+++ customfunction/functions.py
@@ -1,20 +1,31 @@
-Function as_array(x):
-    If x is scalar:
-        Return x as an array
-    Else:
-        Return x
 
-Class Function:
-    Call(*inputs):
-        Extract data from inputs
-        Compute forward results
-        For each input:
-            Set its creator as this function
-        Store inputs and outputs in the function
-        Return outputs
+import numpy as np
+from customfunction.variable import Variable
 
-    Forward(xs):
-        Placeholder for forward computation
+# Extending the framework to support the operations needed for our example
 
-    Backward(gys):
-        Placeholder for backward computation
+class Function:
+    def __call__(self, *inputs):
+        xs = [x.data for x in inputs]
+        ys = self.forward(*xs)
+        if not isinstance(ys, tuple):
+            ys = (ys,)
+        outputs = [Variable(as_array(y)) for y in ys]
+
+        self.generation = max([x.generation for x in inputs])
+        for output in outputs:
+            output.set_creator(self)
+        self.inputs = inputs
+        self.outputs = outputs
+        return outputs if len(outputs) > 1 else outputs[0]
+
+    def forward(self, xs):
+        raise NotImplementedError()
+
+    def backward(self, gys):
+        raise NotImplementedError()
+        
+def as_array(x):
+    if np.isscalar(x):
+        return np.array(x)
+    return x
