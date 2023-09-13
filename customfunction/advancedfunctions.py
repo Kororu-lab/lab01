607affbe9aebbc68906b94da1f3bef8514d5812a
Kororu <68053894+Kororu-lab@users.noreply.github.com>
2023-09-13 21:43:25 +0900

    Update advancedfunctions.py
    answers

--- customfunction/advancedfunctions.py
+++ customfunction/advancedfunctions.py
@@ -1,35 +1,47 @@
-Class Linear (Inherits from Function):
-    Forward(x, W, b):
-        Compute y = xW + b
-        Return y
+import numpy as np
+from customfunction.functions import Function
+from customfunction.variable import Variable
 
-    Backward(gy):
-        Compute gradients for x, W, and b
-        Return gx, gW, gb
+class Linear(Function):
+    def forward(self, x, W, b):
+        y = np.dot(x, W) + b
+        return y
+    
+    def backward(self, gy):
+        x, W, b = self.inputs[0].data, self.inputs[1].data, self.inputs[2].data
+        gx = np.dot(gy, W.T)
+        gW = np.dot(x.T, gy)
+        gb = np.sum(gy, axis=0)
+        return gx, gW, gb
 
-Function linear(x, W, b):
-    Call the Linear class with x, W, b
+def linear(x, W, b):
+    return Linear()(x, W, b)
 
-Class ReLU (Inherits from Function):
-    Forward(x):
-        Compute y = max(0, x)
-        Return y
 
-    Backward(gy):
-        Compute gradient for x using gy and x
-        Return gx
+class ReLU(Function):
+    def forward(self, x):
+        y = np.maximum(0, x)
+        return y
+    
+    def backward(self, gy):
+        x = self.inputs[0].data
+        gx = gy * (x > 0)
+        return gx
 
-Function relu(x):
-    Call the ReLU class with x
+def relu(x):
+    return ReLU()(x)
 
-Class MeanSquaredError (Inherits from Function):
-    Forward(y, t):
-        Compute mean squared error between y and t
-        Return error
 
-    Backward(gy):
-        Compute gradient for y and t using gy, y, and t
-        Return gy and -gy
+class MeanSquaredError(Function):
+    def forward(self, y, t):
+        diff = y - t
+        return np.mean(diff ** 2)
+    
+    def backward(self, gy):
+        y, t = self.inputs[0].data, self.inputs[1].data
+        n = len(y)
+        gy = 2 * (y - t) / n
+        return gy, -gy
 
-Function mean_squared_error(y, t):
-    Call the MeanSquaredError class with y and t
+def mean_squared_error(y, t):
+    return MeanSquaredError()(y, t)
